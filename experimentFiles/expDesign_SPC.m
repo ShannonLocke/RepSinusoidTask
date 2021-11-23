6cbcd5d16e2615e73b68ba1a38e583b4964ced60
Shannon Locke <sml726@nyu.edu>
2021-11-23 12:07:29 +0100

    updated task design
    Changes from the pilot version to the current version.

--- experimentFiles/expDesign_SPC.m
+++ experimentFiles/expDesign_SPC.m
@@ -2,26 +2,25 @@
 % Experiment Parameters
 % ---------------------
 %
-% Created by SML October 2021
+% Created by SML October 2021, updated November 2021
 
 % Mode and condition phases;
-expData.expDesign.modePhases = {'training','test'}; % experiment mode in each phase
-expData.expDesign.condPhases = {'',''}; % here, just block number
+expData.expDesign.modePhases = {'training','test','training','test'}; % experiment mode in each phase
+expData.expDesign.condPhases = {'','','',''}; % nothing to note here
 expData.expDesign.nPhase = length(expData.expDesign.modePhases); % number of phases
 expData.expDesign.phaseTracker = zeros([1,expData.expDesign.nPhase]); % set all to incomplete (0)
 expData.expDesign.phaseTimeStamp = {}; % 
-expData.expDesign.feedbackYN = {false,false}; % experiment mode in each phase
-expData.expDesign.confYN = {false,true}; % experiment mode in each phase
-expData.expDesign.nCalibrationRuns = [0 0]; % how many repeats for the blindspot calibration
-nTestBlocks = sum(strcmp(expData.expDesign.modePhases,'test')); % number of test blocks
+expData.expDesign.feedbackYN = {false,false,false,false}; % experiment mode in each phase
+expData.expDesign.confYN = {false,true,false,true}; % experiment mode in each phase
+expData.expDesign.nCalibrationRuns = [0 0 0 0]; % how many repeats for the blindspot calibration
 
 % Number of trajectories & repeats:
 demoTraj = 5; 
 demoRepeats = 1;
 trainingTraj = 5;
 trainingRepeats = 1; 
-testTraj = 20;
-testRepeats = 4; % 10; % test
+testTraj = 10;
+testRepeats = 20;
 
 % Make design matrices:
 if experimenterYN
@@ -37,20 +36,24 @@ if experimenterYN
             expData.expDesign.designMat{1} = makeDesignMat(conditions,testRepeats/2);
     end
 else
-    conditions = {[1:trainingTraj], [1]};
-    expData.expDesign.designMat{1} = makeDesignMat(conditions,trainingRepeats); % training
-    conditions = {[1:testTraj], [-1,1]};
-    for block = 1:nTestBlocks
-        expData.expDesign.designMat{1+block} = makeDesignMat(conditions,testRepeats/2); % test
+    for ii = 1:expData.expDesign.nPhase
+        conditions_training = {[1:trainingTraj], [1]};
+        conditions_test = {[1:testTraj], [-1,1]};
+        switch expData.expDesign.modePhases{ii}
+            case 'training'
+                expData.expDesign.designMat{ii} = makeDesignMat(conditions_training, trainingRepeats);
+            case 'test'
+                expData.expDesign.designMat{ii} = makeDesignMat(conditions_test, testRepeats/2); % test
+        end
     end
 end
-expData.expDesign.designMat_Legend = {'No conditions'};
+expData.expDesign.designMat_Legend = {'Trajectory ID (1-10)', 'Direction (1: normal, -1: reversed)'};
 
 % Breaks, calibration, and phase progression:
 modesWithBreak = {'test'}; % modes that are long enough for breaks
 breaksEvery = 10; % how frequently to offer a short pause
-forbidRepeatPhase = {}; % prevent from repeating
-calibEvery = 200; % how frequently to calibrate the eye tracker
+forbidRepeatPhase = {'test'}; % prevent from repeating
+calibEvery = 250; % how frequently to re-calibrate the eye tracker
 
 % Pre-allocate response storage:
 expData.res.resp = {}; % confidence response
