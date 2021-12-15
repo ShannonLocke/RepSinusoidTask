8c304841e8cb6875f7bb9ad0dda54158d59a2b41
Shannon Locke <sml726@nyu.edu>
2021-12-15 14:17:38 +0100

    skeleton of DA pipeline

---
+++ dataAnalysis/run_dataAnalysisPipeline.m
@@ -0,0 +1,43 @@
+function [] = run_dataAnalysisPipeline()
+% This script will run the entire data analysis pipeline for the 
+% Repeated-Sinusoids task in the Metacognition for Eye Tracking project. 
+% (Locke, Goettker, Gegenfurtner, & Mamassian, 2021)
+% 
+% Data: https://osf.io/j9asn/
+% Code: https://github.com/ShannonLocke/RepSinusoidTask
+%
+% Created by SML Dec 2021
+% License: CC-By Attribution 4.0 International
+
+% Find the participant IDs of available data:
+sIDs = [];
+
+% Extract the raw data:
+disp('...EXTRACTING THE RAW DATA...')
+prep_extractRawData(sIDs)
+
+% Process the raw data:
+disp('...PROCESSING THE RAW DATA...')
+prep_processData(sIDs)
+
+% Compute data summary:
+disp('...COMPUTING DATA SUMMARY...')
+prep_summaryData(sIDs)
+
+% H1 analysis:
+disp('...TESTING HYPOTHESIS 1 NOW...')
+H1_metacogSensitivity
+
+% H2 analysis:
+disp('...TESTING HYPOTHESIS 2 NOW...')
+H2_repeatedTrajectories
+
+% H3 analysis:
+disp('...TESTING HYPOTHESIS 3 NOW...')
+H3_temporalMetacognitiveSensitivity
+
+% H4 analysis:
+disp('...TESTING HYPOTHESIS 4 NOW...')
+H4_repeatedSessions
+
+end
